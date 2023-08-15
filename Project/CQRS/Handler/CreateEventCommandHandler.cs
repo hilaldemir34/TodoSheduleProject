62b4c0867c637c184fc7fff62e7d22bfab56ec86
LENOVO <LENOVO@DESKTOP-VHTU727>
2023-08-15 09:27:21 +0300

    Proje dosyası ekle.

---
+++ Project/CQRS/Handler/CreateEventCommandHandler.cs
@@ -0,0 +1,24 @@
+﻿using Project.Models;
+
+namespace Project.CQRS.Handler
+{
+    public class CreateEventCommandHandler
+    {
+        private readonly CalendarContext _calendarContext;
+        public CreateEventCommandHandler(CalendarContext calendarContext)
+        {
+            _calendarContext = calendarContext;
+        }
+        public void Handle(Event @event)
+        {
+            _calendarContext.Events.Add(new Event
+            {
+                Color = @event.Color,
+                End= @event.End,    
+                Start = @event.Start,   
+                Text = @event.Text
+            });    
+            _calendarContext.SaveChanges(); 
+        }
+    }
+}
---
+++ Project/CQRS/Handler/GetEventQueryHandler.cs
@@ -0,0 +1,20 @@
+﻿using Project.Models;
+
+namespace Project.CQRS.Handler
+{
+    public class GetEventQueryHandler
+    {
+        private readonly CalendarContext _context;
+
+        public GetEventQueryHandler(CalendarContext context)
+        {
+            _context = context;
+        }
+        public IQueryable<Event> Handle(DateTime start,DateTime end) 
+        {
+            var values = _context.Events
+                   .Where(e => !((e.End <= start) || (e.Start >= end)));   
+            return values;
+        }
+    }
+}
